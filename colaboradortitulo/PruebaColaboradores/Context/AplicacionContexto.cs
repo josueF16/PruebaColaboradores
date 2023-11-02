28dbe8138cda55725cbf30658aeb3c4203df3a69
josueF16 <chinod045@gmail.com>
2023-11-02 12:40:10 -0400

    agregado carpeta sahian

---
+++ colaboradortitulo/PruebaColaboradores/Context/AplicacionContexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.context
+{
+    public class AplicacionContexto : DbContext
+    {
+        public AplicacionContexto
+            (DbContextOptions<AplicacionContexto> options)
+            : base(options) { }
+        public DbSet<Titulo> Titulos { get; set; }
+    }
+}
---
+++ colaboradortitulo/PruebaColaboradores/Models/Titulo.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Titulo
+    {
+        [Key]
+        public int IdTitulo { get; set; }
+        public string titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int IdEmpleado { get; set; }
+
+
+    }
+}

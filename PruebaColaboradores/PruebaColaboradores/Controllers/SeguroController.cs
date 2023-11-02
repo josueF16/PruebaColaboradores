d76216d2db6ad5e6038dcede9aa0179f48c5d034
Julieta Noemi Rocha Cossio <mrspicklesv@gmail.com>
2023-11-01 20:16:41 -0400

    Primera subida seguro

--- PruebaColaboradores/PruebaColaboradores/Controllers/SeguroController.cs
+++ PruebaColaboradores/PruebaColaboradores/Controllers/SeguroController.cs
@@ -6,12 +6,12 @@ namespace WebApplication2.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class DepartamentoController : ControllerBase
+    public class SeguroController : ControllerBase
     {
-        private readonly ILogger<DepartamentoController> _logger;
+        private readonly ILogger<SeguroController> _logger;
         private readonly AplicacionContexto _aplicacionContexto;
-        public DepartamentoController(
-            ILogger<DepartamentoController> logger,
+        public SeguroController(
+            ILogger<SeguroController> logger,
             AplicacionContexto aplicacionContexto)
         {
             _logger = logger;
@@ -21,42 +21,42 @@ namespace WebApplication2.Controllers
         //[Route("")]
         [HttpPost]
         public IActionResult Post(
-            [FromBody] Departamento departamento)
+            [FromBody] Seguro seguro)
         {
-            _aplicacionContexto.Departamentos.Add(departamento);
+            _aplicacionContexto.Seguros.Add(seguro);
             _aplicacionContexto.SaveChanges();
-            return Ok(departamento);
+            return Ok(seguro);
         }
         //READ: Obtener lista de estudiantes
         //[Route("")]
         [HttpGet]
 
-        public IEnumerable<Departamento> Get()
+        public IEnumerable<Seguro> Get()
         {
-            return _aplicacionContexto.Departamentos.ToList();
+            return _aplicacionContexto.Seguros.ToList();
         }
 
         //Update: Modificar estudiantes
         //[Route("/id")]
         [HttpPut]
-        public IActionResult Put([FromBody] Departamento departamento)
+        public IActionResult Put([FromBody] Seguro seguro)
         {
-            _aplicacionContexto.Departamentos.Update(departamento);
+            _aplicacionContexto.Seguros.Update(seguro);
             _aplicacionContexto.SaveChanges();
-            return Ok(departamento);
+            return Ok(seguro);
 
         }
         //Delete: Eliminar estudiantes
         //[Route("/id")]
         [HttpDelete]
-        public IActionResult Delete(int departamentoID)
+        public IActionResult Delete(int seguroID)
         {
-            _aplicacionContexto.Departamentos.Remove(
-                _aplicacionContexto.Departamentos.ToList()
-                .Where(x => x.IdDepartamento == departamentoID)
+            _aplicacionContexto.Seguros.Remove(
+                _aplicacionContexto.Seguros.ToList()
+                .Where(x => x.IdSeguro == seguroID)
                 .FirstOrDefault());
             _aplicacionContexto.SaveChanges();
-            return Ok(departamentoID);
+            return Ok(seguroID);
         }
     }
 }
